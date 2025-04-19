3c24c19efe1713ce397ed1e55206beaaedd53aea
Sanupa <sanuparanmeth@gmail.com>
2025-04-19 21:15:59 +0530

    Designed CustomerView and DriverView - May be changed later

--- src/main/java/views/CustomerView.java
+++ src/main/java/views/CustomerView.java
@@ -5,6 +5,18 @@ import javax.swing.*;
 public class CustomerView extends JFrame {
 
     private JLabel lblWelcome;
+    private JTable tableCustomerShipments;
+    private JPanel containerButton;
+    private JButton btnTrackStatus;
+    private JButton btnCancel;
+    private JButton btnUpdateShipment;
+    private JTextField txtSender;
+    private JTextArea txtAreaDestination;
+    private JTextField txtPkgContents;
+    private JComboBox dropdownTimeSlot;
+    private JCheckBox urgentDeliveryCheckBox;
+    private JButton btnAddShipment;
+    private JPanel customerBackPanel;
 
     public CustomerView(String username) {
 
--- src/main/java/views/DriverView.java
+++ src/main/java/views/DriverView.java
@@ -4,6 +4,14 @@ import javax.swing.*;
 
 public class DriverView extends JFrame {
     private JPanel driverBackPanel;
+    private JLabel lblWelcome;
+    private JTable tableDriverAssignedShipments;
+    private JPanel containerButton;
+    private JButton updateLocationButton;
+    private JButton delayButton;
+    private JButton updateStatusButton;
+    private JButton updateEstimationButton;
+    private JButton viewHistoryButton;
 
     public DriverView(String username) {
 
