4c76582f4a60a8a1d5607332cd5c0e3493cf8b67
Sanupa <sanuparanmeth@gmail.com>
2025-06-09 13:56:10 +0530

    Added View Notification button to driver view.

--- src/main/java/views/DriverView.java
+++ src/main/java/views/DriverView.java
@@ -1,6 +1,8 @@
 package views;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class DriverView extends JFrame {
     private JPanel driverBackPanel;
@@ -12,11 +14,21 @@ public class DriverView extends JFrame {
     private JButton updateStatusButton;
     private JButton updateEstimationButton;
     private JButton viewHistoryButton;
+    private JButton viewNotificationsButton;
 
     public DriverView(String username) {
         setContentPane(driverBackPanel);
         setTitle("FastTrack Logistics - Driver Dashboard");
         setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Open notification panel when viewNotification button is clicked
+        viewNotificationsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new NotificationView(username).setVisible(true);
+                dispose();
+            }
+        });
     }
 }
