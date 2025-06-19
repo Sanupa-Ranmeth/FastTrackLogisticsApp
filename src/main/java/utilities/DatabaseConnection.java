064f43fa864440e1c01fc099ddd673494aaa7e6f
Sanupa <sanuparanmeth@gmail.com>
2025-06-19 19:59:18 +0530

    Changed database connection to localhost (Commented out remote DB connection)

--- src/main/java/utilities/DatabaseConnection.java
+++ src/main/java/utilities/DatabaseConnection.java
@@ -5,10 +5,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
+    /*
+    Remote Connection URL
 
     private static final String USER = "root";
     private static final String PASSWORD = "#28@Vihanga#Jay.";
     private static final String URL = "jdbc:mysql://sql.vihaga.dev:3306/FastTrackLogisticsDB?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+     */
+
+    //Localhost URL
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+    private static final String URL = "jdbc:mysql://localhost:3306/fasttracklogisticsdb";
 
     public static Connection getConnection() {
         try {
